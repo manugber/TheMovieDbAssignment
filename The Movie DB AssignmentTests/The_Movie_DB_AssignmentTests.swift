191609f39477a8db473692c50d1e0990dd83d267
Manuel Gonzalez Benaldez <manuel.gonzalez@edataconsulting.de>
2024-11-12 11:21:46 +0000

    Initial Commit

---
+++ The Movie DB AssignmentTests/The_Movie_DB_AssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  The_Movie_DB_AssignmentTests.swift
+//  The Movie DB AssignmentTests
+//
+//  Created by Manuel Gonzalez Bernaldez on 12/11/24.
+//
+
+import Testing
+@testable import The_Movie_DB_Assignment
+
+struct The_Movie_DB_AssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
